143f430dbe256b2ab314e586a9bf71aebfc3ad13
tykyphatky <gg.fctaiphat@yahoo.com>
2025-07-09 19:03:13 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,19 @@ import LoginPage from './pages/LoginPage/LoginPage';
 import PatientHomepage from './pages/Patient/PatientHomePage';
 import AboutPage from './pages/About/AboutPage';
 import ContactPage from './pages/Contact/ContactPage';
+import PatientBooking from './pages/Patient/PatientBookingPage';
 
 function App() {
   return (
     <AuthProvider>
       <Router>
-        <Routes>
-
+        <Routes>       
           <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<Homepage />} />
           <Route path="/home" element={<PatientHomepage />} />
           <Route path="/about-us" element={<AboutPage />} />
           <Route path="/contact-us" element={<ContactPage />} />
+          <Route path="/patient/booking" element={<PatientBooking />} />
           <Route 
             path="/" 
             element={
