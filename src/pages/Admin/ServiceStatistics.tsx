d265228230a907b3c482231d8282af3a72cfd3cc
tykyphatky <gg.fctaiphat@yahoo.com>
2025-07-12 13:44:35 +0700

    Update ServiceStatistics.tsx

--- src/pages/Admin/ServiceStatistics.tsx
+++ src/pages/Admin/ServiceStatistics.tsx
@@ -182,9 +182,8 @@ const ServiceStatistics: React.FC = () => {
                       cx="50%"
                       cy="50%"
                       labelLine={false}
-                      label={({ name, percent }: { name: string; percent: number }) => `${name} ${(percent * 100).toFixed(0)}%`
+                      label={({ name, percent }: { name?: string; percent?: number }) => `${name ?? 'N/A'} ${(percent !== undefined ? (percent * 100).toFixed(0) : '0')}%`             
                     }
-
                       outerRadius={80}
                       fill="#8884d8"
                       dataKey="value"
